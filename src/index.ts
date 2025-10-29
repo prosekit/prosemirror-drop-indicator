303a9bfe6e1e2e69915f09489da72aae4c2582b3
ocavue <ocavue@users.noreply.github.com>
2025-10-29 16:42:36 +1100

    feat: add drop indicator plugin (#2)
    Release-as: 0.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-export const one = 1
-export const two = 2
+export {
+  createDropIndicatorPlugin,
+  type DropIndicatorPluginOptions,
+} from './drop-indicator-plugin'
+export type {
+  DragEventHandler,
+  DragEventHandlerOptions,
+  Line,
+  Point,
+  ShowHandler,
+  ShowHandlerOptions,
+  ViewDragging,
+} from './types'
